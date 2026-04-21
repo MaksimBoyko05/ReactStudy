a49523e70815dd59f206f74ff316243f7b170f77
MaksimBoyko05 <vladosmakasin1@gmail.com>
2026-04-21 17:09:52 +0300

    Loops
    -map to create a new array from old one + discount.
    -example of loop for...of

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import {useContext, useState} from 'react';
+import {useContext} from 'react';
 import Navbar from './components/Navbar';
-import {ThemeContext, ThemeProvider} from "./context/ThemeContext";
+import {ThemeContext} from "./context/ThemeContext";
 import Counter from "./components/Counter";
+import Cart from "./components/Cart";
 
 export default function App() {
-const {theme} = useContext(ThemeContext)
+  const {theme} = useContext(ThemeContext)
 
   return (
-    <div style={{ background: theme === 'light' ? '#fff' : '#333', height: '100vh' }}>
-      <Navbar />
-      <h1 style={{ color: theme === 'light' ? '#000' : '#fff' }}>Головна сторінка</h1>
+    <div style={{background: theme === 'light' ? '#fff' : '#333', height: '100vh'}}>
+      <Navbar/>
+      <h1 style={{color: theme === 'light' ? '#000' : '#fff'}}>Головна сторінка</h1>
       <Counter/>
+      <Cart/>
     </div>
   );
 }
---
+++ src/components/Cart.jsx
@@ -0,0 +1,32 @@
+export default function Cart() {
+  const cart = [
+    {id: 1, name: "Ноутбук", price: 1000},
+    {id: 2, name: "Мишка", price: 50},
+    {id: 3, name: "Клавіатура", price: 100}
+  ];
+  const discountCart = cart.map(product => {
+    return {...product, price: product.price * 0.9}
+  })
+  const messages = ['Завантаження', 'Підключення', 'Доступ заборонено', 'Отримання даних'];
+  for (const message of messages) {
+    if (message === 'Доступ заборонено') {
+      console.log('Доступ заборонено!')
+      break;
+    }
+    console.log(message)
+  }
+
+  return (
+    <>
+      <div>
+        <ul>
+          {discountCart.map(prod => (
+            <li>
+              {prod.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
